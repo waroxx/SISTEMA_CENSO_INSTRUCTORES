493a6603e0be2adc8fe94865c2b2f7617c118bc3
EBECKFORD <warobeck7@gmail.com>
2019-09-18 11:45:30 -0500

    first commit

---
+++ SISTEMA_CENSO_INSTRUCTORES/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SISTEMA_CENSO_INSTRUCTORES.Global" Language="C#" %>
---
+++ SISTEMA_CENSO_INSTRUCTORES/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SISTEMA_CENSO_INSTRUCTORES.Startup))]
+namespace SISTEMA_CENSO_INSTRUCTORES
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
