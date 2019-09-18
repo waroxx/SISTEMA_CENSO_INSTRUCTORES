ad86d4a3c9b09bec7ea1205105e1ca65a1521c73
EBECKFORD <warobeck7@gmail.com>
2019-09-18 14:45:44 -0500

    dbcons

---
+++ SISTEMA_CENSO_INSTRUCTORES/utilidades/DBConnections.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SISTEMA_CENSO_INSTRUCTORES.utilidades
+{
+    public class DBConnections
+    {
+    }
+}
